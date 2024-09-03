471bc50263616a09220b96e0c7bc9977ff0fee79
Carlos Alves <calves1997@hotmail.com>
2024-09-03 20:03:06 +0100

    ...

--- src/main/java/org/calves/fnzs/controller/FnzsController.java
+++ src/main/java/org/calves/fnzs/controller/FnzsController.java
@@ -1,22 +1,16 @@
 package org.calves.fnzs.controller;
 
-import api.EpicGamesApi;
 import controller.AccountsController;
 import controller.MongoDbController;
 import dto.canonical.Account;
-import dto.epicgames.SearchAccountsResponse;
-import exception.AccountNotFoundException;
-import okhttp3.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.calves.yunite4j.YuniteApi;
 import org.calves.yunite4j.dto.ApiConfig;
 import org.calves.yunite4j.dto.Team;
 import org.calves.yunite4j.dto.Tournament;
-import utils.DeserializationUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -50,23 +44,21 @@ public class FnzsController {
         LOGGER.info("Retrieved a total of {} teams", teams.size());
 
         // todo: fix missing names
-        // First we enrich data with Epic usernames
 
+        // First we enrich data with Epic usernames
         LOGGER.info("Enriching data Epic usernames");
         for (Team team : teams) {
             for (Team.User user : team.getUsers()) {
-                if (user.getEpicUsername() == null) {
-                    LOGGER.debug("Trying to enrich username for account {}", user.getEpicId());
-                    try {
-                        user.setEpicUsername(lolgetUsernameFromAccountId(user.getEpicId()));
-                        if (user.getEpicUsername() == null || user.getEpicUsername().isBlank()) {
-                            LOGGER.warn("Username for {} is NULL or BLANK: ", user.getEpicId(), user.getEpicUsername());
-                            user.setEpicUsername(String.format("Unknown(%s)", user.getEpicId()));
-                        }
-                    } catch (Exception ex) {
-                        LOGGER.warn("Failed to update username for {}", user.getEpicId(), ex);
-                    }
+                Account account = Account.builder().username(String.format("Unknown(%s)", user.getEpicId())).build();
+                try {
+                    account = MongoDbController.getAccount(user.getEpicId());
+                } catch (Exception ex) {
+                    System.out.println();
                 }
+                if (account == null) {
+                    account = AccountsController.insertAccountWithId(user.getEpicId(), null, null);
+                }
+                user.setEpicUsername(account.getUsername());
             }
         }
 
@@ -79,27 +71,6 @@ public class FnzsController {
         LOGGER.debug("Finished sorting leaderboard");
 
         return resultingTeams;
-
-        // DiscordNotificationUtils.logRanking(tournament, resultingTeams);
-    }
-
-    public static String lolgetUsernameFromAccountId(String accountId) throws AccountNotFoundException {
-        Account account = MongoDbController.getAccount(accountId);
-        if (account != null && account.getUsername() != null && !account.getUsername().isEmpty()) {
-            LOGGER.info("Account from database already contains a username, returning that");
-            return account.getUsername();
-        } else {
-            LOGGER.info("Looking for username for {} in Epic Games API", accountId);
-            Response response = EpicGamesApi.getDisplayNames(Collections.singletonList(accountId));
-            ArrayList<SearchAccountsResponse> searchAccountsResponse = DeserializationUtils.convertResponseToList(response, SearchAccountsResponse.class);
-            LOGGER.info("Response contains {} results", searchAccountsResponse.size());
-            if (searchAccountsResponse.size() > 1) {
-                LOGGER.error("Found more than one account with ID {} on Epic Games API", accountId);
-            } else if (searchAccountsResponse.isEmpty()) {
-                throw new AccountNotFoundException();
-            }
-            return searchAccountsResponse.getFirst().findName();
-        }
     }
 
     public static List<Team> splitAndMergeTeams(List<Team> teams) {
