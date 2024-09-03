9cd4b3b9299f049a8e30f23947d8e8eed67b3f85
Carlos Alves <calves1997@hotmail.com>
2024-09-03 12:00:44 +0100

    Basically deprecated RestContoller

--- src/main/java/org/calves/fnzs/controller/RestController.java
+++ src/main/java/org/calves/fnzs/controller/RestController.java
@@ -1,5 +1,7 @@
 package org.calves.fnzs.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class RestController {
 
     private static final String ERROR_MESSAGE = "Error, reach out to calves07 on Discord";
+    private static final Logger LOGGER = LogManager.getLogger(RestController.class);
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello world";
-    }
+    //   @GetMapping("/")
+    //   public String helloWorld() {
+    //   return "Hello world";
+    //   }
+
+    /*
 
     @GetMapping("/key")
     public int key() {
@@ -20,8 +25,11 @@ public class RestController {
 
     @GetMapping("/{guildId}/leaderboard/{tournamentId}/individual")
     public String getIndividualLeaderboard(@PathVariable String guildId, @PathVariable String tournamentId) {
-        FnzsController.getLeaderboard(guildId, tournamentId);
+        LOGGER.debug("Received request to get individual leaderboard for guild {} and tournament {}", guildId, tournamentId);
+        // FnzsController.getTournamentLeaderboard(guildId, tournamentId);
         return "lorem ipsum dolor sit amet";
     }
+
+     */
 
 }
