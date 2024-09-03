1d966da4d2eade020f40f2a65fc465f8f3fa75ad
Carlos Alves <calves1997@hotmail.com>
2024-09-03 12:00:08 +0100

    Added vaadin dependency and WIP changes to discord embeds

--- src/main/java/org/calves/fnzs/utils/DiscordNotificationUtils.java
+++ src/main/java/org/calves/fnzs/utils/DiscordNotificationUtils.java
@@ -1,4 +1,4 @@
-package org.calves.fnzs;
+package org.calves.fnzs.utils;
 
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.WebhookClientBuilder;
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.calves.yunite4j.dto.Team;
 import org.calves.yunite4j.dto.Tournament;
+import utils.MathUtils;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -31,13 +32,19 @@ public class DiscordNotificationUtils {
         StringBuilder sb = new StringBuilder();
         WebhookEmbed.EmbedFooter footer = new WebhookEmbed.EmbedFooter("Developed by @calves07", "https://cdn.discordapp.com/icons/1213253795333541960/37779228e58038571549819a2c6aa362.png");
         footer = null;
+       // int count = 0;
+       sb.append(String.format("```%-3s %-16s %-5s %-5s %-5s %-5s```", "#", "Username", "Kills", "Wins", "AvgP", "Score"));
         for (Team team : teams) {
-            sb.append(String.format("```%-3s %-30s %-3d```", team.getPlacement(), team.getUsers().getFirst().getEpicUsername(), team.getScore()));
+            //if(count < 20){
+                sb.append(String.format("```%-3s %-16s %-5s %-5s %-5s %-5s```", team.getPlacement(), team.getUsers().getFirst().getEpicUsername(), team.getKills(), team.getWins(), MathUtils.roundToDecimalPlaces(team.getAveragePlacement(), 1), team.getScore()));
+         //   }
+          //  count++;
             // sb.append(String.format("\n%d.  %s, %d points", team.getPlacement(), team.getUsers().getFirst().getEpicUsername(), team.getScore()));
         }
         WebhookEmbed embed = new WebhookEmbed(OffsetDateTime.now(), EMBED_COLOR, sb.toString(), THUMBNAIL_URL,
                 null, footer, new WebhookEmbed.EmbedTitle(tournament.getName(),
                 String.format("https://yunite.xyz/leaderboard/%s", tournament.getId())), null, new ArrayList<>());
+
         CLIENT.send(embed);
     }
 
